7fa57269809184784e516155e96d8fa820c6f477
Ömer Algin <oemer-algin@hotmail.de>
2022-09-18 22:26:28 +0200

    delete unnecessary files

---
+++ src/components/MusicPlayer.js
@@ -0,0 +1,81 @@
+export default function MusicPlayer() {
+	return (
+		<div>
+			<main>Hallo</main>
+		</div>
+	);
+}
+
+// import {useState} from 'react';
+
+// function MusicPlayer() {
+// 	const [songs] = useState([
+// 		{
+// 			id: 1,
+// 			title: 'Beat 1',
+// 			producer: 'Unknown',
+// 			src: './beats/beat1.mp3',
+// 		},
+// 		{
+// 			id: 2,
+// 			title: 'Beat 2',
+// 			producer: 'Unknown',
+// 			src: './beats/beat2.mp3',
+// 		},
+// 		{
+// 			id: 3,
+// 			title: 'Beat 3',
+// 			producer: 'Unknown',
+// 			src: './beats/beat3.mp3',
+// 		},
+// 		{
+// 			id: 4,
+// 			title: 'Beat 4',
+// 			producer: 'Unknown',
+// 			src: './beats/beat4.mp3',
+// 		},
+// 		{
+// 			id: 5,
+// 			title: 'Beat 5',
+// 			producer: 'Unknown',
+// 			src: './beats/beat5.mp3',
+// 		},
+// 		{
+// 			id: 6,
+// 			title: 'Beat 6',
+// 			producer: 'Unknown',
+// 			src: './beats/beat6.mp3',
+// 		},
+// 		{
+// 			id: 7,
+// 			title: 'Beat 7',
+// 			producer: 'Unknown',
+// 			src: './beats/beat7.mp3',
+// 		},
+// 		{
+// 			id: 8,
+// 			title: 'Beat 8',
+// 			producer: 'Unknown',
+// 			src: './beats/beat8.mp3',
+// 		},
+// 		{
+// 			id: 9,
+// 			title: 'Beat 9',
+// 			producer: 'Unknown',
+// 			src: './beats/beat9.mp3',
+// 		},
+// 		{
+// 			id: 10,
+// 			title: 'Beat 10',
+// 			producer: 'Unknown',
+// 			src: './beats/beat10.mp3',
+// 		},
+// 	]);
+
+// 	return (
+// 		<div>
+// 			<p>{songs} Hallo</p>
+// 		</div>
+// 	);
+// }
+// export default MusicPlayer;
