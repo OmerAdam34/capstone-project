513424e8c1bd8aeb49291b9509f6da87e6f59ca7
Ömer Algin <oemer-algin@hotmail.de>
2022-09-19 13:50:52 +0200

    add tracks, important components for the playbutton function

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,81 +1,84 @@
+import {useState} from 'react';
+import {useEffect} from 'react';
+
+import Player from './Player';
+
 export default function MusicPlayer() {
+	const [songs] = useState([
+		{
+			id: 1,
+			title: 'Beat 1',
+			producer: 'Unknown',
+			src: './beats/beat1.mp3',
+		},
+		{
+			title: 'Beat 2',
+			producer: 'Unknown',
+			src: './beats/beat2.mp3',
+		},
+		{
+			title: 'Beat 3',
+			producer: 'Unknown',
+			src: './beats/beat3.mp3',
+		},
+		{
+			title: 'Beat 4',
+			producer: 'Unknown',
+			src: './beats/beat4.mp3',
+		},
+		{
+			title: 'Beat 5',
+			producer: 'Unknown',
+			src: './beats/beat5.mp3',
+		},
+		{
+			title: 'Beat 6',
+			producer: 'Unknown',
+			src: './beats/beat6.mp3',
+		},
+		{
+			title: 'Beat 7',
+			producer: 'Unknown',
+			src: './beats/beat7.mp3',
+		},
+		{
+			title: 'Beat 8',
+			producer: 'Unknown',
+			src: './beats/beat8.mp3',
+		},
+		{
+			title: 'Beat 9',
+			producer: 'Unknown',
+			src: './beats/beat9.mp3',
+		},
+		{
+			title: 'Beat 10',
+			producer: 'Unknown',
+			src: './beats/beat10.mp3',
+		},
+	]);
+
+	const [currentSongIndex, setCurrentSongIndex] = useState(0);
+	const [nextSongIndex, setNextSongIndex] = useState(0);
+
+	useEffect(() => {
+		setNextSongIndex(() => {
+			if (currentSongIndex + 1 > songs.length - 1) {
+				return 0;
+			} else {
+				return currentSongIndex + 1;
+			}
+		});
+	}, [currentSongIndex]);
+
 	return (
 		<div>
-			<main>Hallo</main>
+			<Player
+				currentSongIndex={currentSongIndex}
+				setCurrentSongIndex={setCurrentSongIndex}
+				nextSongIndex={nextSongIndex}
+				songs={songs}
+			/>
 		</div>
 	);
 }
-
-// import {useState} from 'react';
-
-// function MusicPlayer() {
-// 	const [songs] = useState([
-// 		{
-// 			id: 1,
-// 			title: 'Beat 1',
-// 			producer: 'Unknown',
-// 			src: './beats/beat1.mp3',
-// 		},
-// 		{
-// 			id: 2,
-// 			title: 'Beat 2',
-// 			producer: 'Unknown',
-// 			src: './beats/beat2.mp3',
-// 		},
-// 		{
-// 			id: 3,
-// 			title: 'Beat 3',
-// 			producer: 'Unknown',
-// 			src: './beats/beat3.mp3',
-// 		},
-// 		{
-// 			id: 4,
-// 			title: 'Beat 4',
-// 			producer: 'Unknown',
-// 			src: './beats/beat4.mp3',
-// 		},
-// 		{
-// 			id: 5,
-// 			title: 'Beat 5',
-// 			producer: 'Unknown',
-// 			src: './beats/beat5.mp3',
-// 		},
-// 		{
-// 			id: 6,
-// 			title: 'Beat 6',
-// 			producer: 'Unknown',
-// 			src: './beats/beat6.mp3',
-// 		},
-// 		{
-// 			id: 7,
-// 			title: 'Beat 7',
-// 			producer: 'Unknown',
-// 			src: './beats/beat7.mp3',
-// 		},
-// 		{
-// 			id: 8,
-// 			title: 'Beat 8',
-// 			producer: 'Unknown',
-// 			src: './beats/beat8.mp3',
-// 		},
-// 		{
-// 			id: 9,
-// 			title: 'Beat 9',
-// 			producer: 'Unknown',
-// 			src: './beats/beat9.mp3',
-// 		},
-// 		{
-// 			id: 10,
-// 			title: 'Beat 10',
-// 			producer: 'Unknown',
-// 			src: './beats/beat10.mp3',
-// 		},
-// 	]);
-
-// 	return (
-// 		<div>
-// 			<p>{songs} Hallo</p>
-// 		</div>
-// 	);
-// }
-// export default MusicPlayer;
