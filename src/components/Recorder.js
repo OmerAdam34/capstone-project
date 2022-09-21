36a757fc1b380c805bfbc196369420ecc5f01f95
Ömer Algin <oemer-algin@hotmail.de>
2022-09-21 10:20:33 +0200

    recorder component and icons for the recorder

---
+++ src/components/Recorder.js
@@ -0,0 +1,12 @@
+import {BsRecordCircleFill} from 'react-icons/bs';
+
+function Recorder() {
+	return (
+		<div>
+			<button>
+				<BsRecordCircleFill />
+			</button>
+		</div>
+	);
+}
+export default Recorder;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import MusicPlayer from '../components/MusicPlayer';
+import Recorder from '../components/Recorder';
 
 export default function HomePage() {
 	return (
 		<div>
 			<MusicPlayer />
+			<Recorder />
 		</div>
 	);
 }
