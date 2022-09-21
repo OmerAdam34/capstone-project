a5869813350a037f0cc552948146ce4b34d1914b
Ömer Algin <oemer-algin@hotmail.de>
2022-09-21 15:14:33 +0200

    create recording function

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -1,12 +1,55 @@
-import {BsRecordCircleFill} from 'react-icons/bs';
+import MicRecorder from 'mic-recorder-to-mp3';
+import {useEffect, useState, useRef} from 'react';
+import {FaMicrophone} from 'react-icons/fa';
+import {FaMicrophoneAltSlash} from 'react-icons/fa';
 
-function Recorder() {
+export default function Recorder() {
+	const recorder = useRef(null);
+	const audioPlayer = useRef(null);
+
+	const [blobUrl, setBlobUrl] = useState(null);
+	const [audioFile, setAudioFile] = useState(null);
+	const [isRecording, setIsRecording] = useState(null);
+
+	useEffect(() => {
+		recorder.current = new MicRecorder({bitRate: 128});
+	}, []);
+
+	const startRecording = () => {
+		recorder.current.start().then(() => {
+			setIsRecording(true);
+		});
+	};
+
+	const stopRecording = () => {
+		recorder.current
+			.stop()
+			.getMp3()
+			.then(([buffer, blob]) => {
+				const file = new File(buffer, 'audio.mp3', {
+					type: blob.type,
+					lastModified: Date.now(),
+				});
+				const newBlobUrl = URL.createObjectURL(blob);
+				setBlobUrl(newBlobUrl);
+				setIsRecording(false);
+				setAudioFile(file);
+			});
+		console.log(audioFile);
+	};
+
 	return (
 		<div>
-			<button>
-				<BsRecordCircleFill />
-			</button>
+			{!isRecording ? (
+				<button disabled={isRecording} onClick={startRecording}>
+					<FaMicrophone />
+				</button>
+			) : (
+				<button disabled={!isRecording} onClick={stopRecording}>
+					<FaMicrophoneAltSlash />
+				</button>
+			)}
+			<audio ref={audioPlayer} src={blobUrl} controls="controls"></audio>
 		</div>
 	);
 }
-export default Recorder;
