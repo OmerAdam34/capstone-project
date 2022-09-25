9997de20ea33bf677775b5afdd1ed16addbc55cd
Ömer Algin <oemer-algin@hotmail.de>
2022-09-25 16:00:01 +0200

    description field for recordings

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -1,6 +1,7 @@
 import MicRecorder from 'mic-recorder-to-mp3';
 import {nanoid} from 'nanoid';
 import {useEffect, useState, useRef} from 'react';
+import EdiText from 'react-editext';
 import {FaMicrophone} from 'react-icons/fa';
 import {FaMicrophoneAltSlash} from 'react-icons/fa';
 
@@ -8,6 +9,12 @@ export default function Recorder() {
 	const recorder = useRef(null);
 
 	const [isRecording, setIsRecording] = useState(null);
+
+	const [value, setValue] = useState('Give your track a name!');
+
+	const handleSave = value => {
+		setValue(value);
+	};
 
 	const handleAddRecording = (blobUrl, audiofile) => {
 		setAddRecordings([...addRecordings, {id: nanoid(), url: blobUrl, src: audiofile}]);
@@ -55,6 +62,7 @@ export default function Recorder() {
 			<h4>MY RECORDED TRACKS:</h4>
 			{addRecordings.map(addRecording => (
 				<div key={addRecording.id} className="audio-container">
+					<EdiText value={value} type="text" onSave={handleSave} id={nanoid()} />
 					<audio src={addRecording.url} controls="controls"></audio>
 					<button onClick={() => deleteAudio(addRecording.id)}>Delete</button>
 				</div>
