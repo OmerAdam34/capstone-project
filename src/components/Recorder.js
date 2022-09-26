dd1844a63a691bc1113eb4c4c6071ba5e89d65a2
Omer <107583280+OmerAdam34@users.noreply.github.com>
2022-09-26 17:02:23 +0200

    Description (#18)
    * description field for recordings
    
    * landing page description and create link buttons
    
    * track discription
    
    * desription update

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -1,16 +1,27 @@
 import MicRecorder from 'mic-recorder-to-mp3';
 import {nanoid} from 'nanoid';
+import {useRouter} from 'next/router';
 import {useEffect, useState, useRef} from 'react';
+import EdiText from 'react-editext';
 import {FaMicrophone} from 'react-icons/fa';
 import {FaMicrophoneAltSlash} from 'react-icons/fa';
 
 export default function Recorder() {
+	const router = useRouter();
+
+	function handleSubmit() {
+		router.push('/start');
+	}
+
 	const recorder = useRef(null);
 
 	const [isRecording, setIsRecording] = useState(null);
 
 	const handleAddRecording = (blobUrl, audiofile) => {
-		setAddRecordings([...addRecordings, {id: nanoid(), url: blobUrl, src: audiofile}]);
+		setAddRecordings([
+			...addRecordings,
+			{id: nanoid(), url: blobUrl, src: audiofile, description: 'Add your des.'},
+		]);
 	};
 
 	const deleteAudio = id => {
@@ -18,6 +29,18 @@ export default function Recorder() {
 	};
 
 	const [addRecordings, setAddRecordings] = useState([]);
+
+	//const [value, setValue] = useState(['Give your track a name!']);
+
+	const handleSave = (value, id) => {
+		setAddRecordings(
+			addRecordings.map(addRecording => {
+				return addRecording.id === id
+					? {...addRecording, description: value}
+					: addRecording;
+			})
+		);
+	};
 
 	useEffect(() => {
 		recorder.current = new MicRecorder({bitRate: 128});
@@ -55,10 +78,22 @@ export default function Recorder() {
 			<h4>MY RECORDED TRACKS:</h4>
 			{addRecordings.map(addRecording => (
 				<div key={addRecording.id} className="audio-container">
+					<EdiText
+						value={addRecording.description}
+						type="text"
+						onSave={value => handleSave(value, addRecording.id)}
+						id={addRecording.id}
+						editButtonContent={'Title'}
+						editOnViewClick={false}
+						submitOnEnter={true}
+					/>
 					<audio src={addRecording.url} controls="controls"></audio>
 					<button onClick={() => deleteAudio(addRecording.id)}>Delete</button>
 				</div>
 			))}
+			<div>
+				<button onClick={handleSubmit}>SEE INTRODUCTION</button>
+			</div>
 		</div>
 	);
 }
