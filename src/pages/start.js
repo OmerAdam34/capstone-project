acf09354b999e26b3550c231da1ad0e6709cba88
Ömer Algin <oemer-algin@hotmail.de>
2022-10-04 11:51:07 +0200

    parallax and parallaxlayout

--- src/pages/start.js
+++ src/pages/start.js
@@ -1,3 +1,4 @@
+import {Parallax, ParallaxLayer} from '@react-spring/parallax';
 import Link from 'next/link';
 
 export default function start() {
@@ -12,6 +13,34 @@ export default function start() {
 			<button>
 				<Link href="/">START </Link>
 			</button>
+
+			<Parallax pages={2} style={{top: '0', left: '0'}}>
+				<ParallaxLayer
+					offset={0}
+					speed={2.5}
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<h2>1. Choose a beat</h2>
+				</ParallaxLayer>
+				<ParallaxLayer offset={1} speed={2} style={{backgroundColor: '#232323'}} />
+
+				<ParallaxLayer
+					offset={1}
+					speed={0.5}
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						color: 'white',
+					}}
+				>
+					<h2>2. Create your own lyrics</h2>
+				</ParallaxLayer>
+			</Parallax>
 		</div>
 	);
 }
