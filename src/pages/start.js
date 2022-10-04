5caedf61c6bde4425d6e9009ce95341067f3ca28
Ömer Algin <oemer-algin@hotmail.de>
2022-10-04 13:29:38 +0200

    build parallax effect

--- src/pages/start.js
+++ src/pages/start.js
@@ -5,16 +5,8 @@ export default function start() {
 	return (
 		<div>
 			<h1>Introduction</h1>
-			<p>1. Choose a beat </p>
-			<p>2. Create your own lyrics</p>
-			<p>3. Try to sing/rap your lyrics with the beat</p>
-			<p>4. All right, show your skills and record it</p>
-			<p>5. Download and send your own mp3 to someone or collect it</p>
-			<button>
-				<Link href="/">START </Link>
-			</button>
 
-			<Parallax pages={2} style={{top: '0', left: '0'}}>
+			<Parallax pages={5} style={{top: '0', left: '0'}}>
 				<ParallaxLayer
 					offset={0}
 					speed={2.5}
@@ -26,11 +18,12 @@ export default function start() {
 				>
 					<h2>1. Choose a beat</h2>
 				</ParallaxLayer>
+
 				<ParallaxLayer offset={1} speed={2} style={{backgroundColor: '#232323'}} />
 
 				<ParallaxLayer
 					offset={1}
-					speed={0.5}
+					speed={2.5}
 					style={{
 						display: 'flex',
 						justifyContent: 'center',
@@ -39,6 +32,53 @@ export default function start() {
 					}}
 				>
 					<h2>2. Create your own lyrics</h2>
+				</ParallaxLayer>
+
+				<ParallaxLayer offset={2} speed={2} style={{backgroundColor: '#232323'}} />
+
+				<ParallaxLayer
+					offset={2}
+					speed={2.5}
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						color: 'white',
+					}}
+				>
+					<h2>3. Try to sing/rap your lyrics with the beat</h2>
+				</ParallaxLayer>
+
+				<ParallaxLayer offset={3} speed={2} style={{backgroundColor: '#232323'}} />
+
+				<ParallaxLayer
+					offset={3}
+					speed={2.5}
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						color: 'white',
+					}}
+				>
+					<h2>4. All right, show your skills and record it</h2>
+				</ParallaxLayer>
+
+				<ParallaxLayer
+					offset={4}
+					speed={2.5}
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<h2>5. Download and send your own mp3 to someone or collect it</h2>
+					<div>
+						<button>
+							<Link href="/">START </Link>
+						</button>
+					</div>
 				</ParallaxLayer>
 			</Parallax>
 		</div>
