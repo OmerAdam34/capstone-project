3d7d4d57d28a2d16693ab94600d72a888759f1b0
Ömer Algin <oemer-algin@hotmail.de>
2022-10-04 11:12:34 +0200

    install react spring

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 const providers = [];
 if (process.env.VERCEL_ENV === 'preview') {
@@ -28,6 +29,10 @@ if (process.env.VERCEL_ENV === 'preview') {
 		GithubProvider({
 			clientId: process.env.GITHUB_ID,
 			clientSecret: process.env.GITHUB_SECRET,
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		})
 	);
 }
