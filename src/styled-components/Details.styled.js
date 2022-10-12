ea3af9f6d9592b381bcf6c1440f369fba4b9a955
Ömer Algin <oemer-algin@hotmail.de>
2022-10-12 15:21:05 +0200

    styling buttons

--- src/styled-components/Details.styled.js
+++ src/styled-components/Details.styled.js
@@ -5,8 +5,7 @@ export const Details = styled.p`
 	z-index: 2;
 	top: 131px;
 	left: 100px;
-	font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial,
-		sans-serif;
+	font-family: Arial, Helvetica, sans-serif;
 `;
 
 export const Producer = styled(Details)`
