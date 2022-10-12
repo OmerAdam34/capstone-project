fd420ff4d7f4f797aed0fca5735f06abfbf3f674
Ömer Algin <oemer-algin@hotmail.de>
2022-10-12 14:03:40 +0200

    inco. page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,38 @@
 import {useSession} from 'next-auth/react';
+import Image from 'next/image';
 import {useState} from 'react';
+import styled from 'styled-components';
 
+import Cassette from '../../public/images/cassette.png';
+import Logo2 from '../../public/images/logo-white.png';
 import MusicPlayer from '../components/MusicPlayer';
 import Recorder from '../components/Recorder';
-import {Button} from '../styled-components/Button.styled';
+import {IncognitoButton} from '../styled-components/Button.styled';
+import {IncognitoHeadline} from '../styled-components/Headline.styled';
+import {IncognitoContainer} from '../styled-components/Incognito.styled';
+import {IncognitoBox} from '../styled-components/IncognitoBox.styled';
+import {LoginPage} from '../styled-components/LoginPage.styled';
+
+const LogoWhite = styled.div`
+	position: absolute;
+	top: -30px;
+	right: 0;
+	left: 0;
+	width: 100%;
+	height: auto;
+	background-color: black;
+	box-shadow: rgba(0, 0, 0, 0.56) 0 22px 70px 4px;
+`;
+
+const CassetteImg = styled.div`
+	position: absolute;
+	bottom: 10%;
+	left: 14%;
+	width: 250px;
+	height: auto;
+	padding: 10px 10px 10px 10px;
+	object-fit: cover;
+`;
 
 export default function HomePage() {
 	const {data: session} = useSession();
@@ -15,14 +44,27 @@ export default function HomePage() {
 		</div>
 	) : (
 		<div>
-			<h1>Please login</h1>
-			<Button
-				onClick={() => {
-					setShowApp(true);
-				}}
-			>
-				CONTINUE WITHOUT LOGIN
-			</Button>
+			<LoginPage>
+				<LogoWhite>
+					<Image src={Logo2} width="1000" height="1000" alt="logo" objectFit="cover" />
+				</LogoWhite>
+				<IncognitoContainer>
+					<IncognitoHeadline>PLEASE LOGIN</IncognitoHeadline>
+				</IncognitoContainer>
+
+				<CassetteImg>
+					<Image src={Cassette} width="1307" height="840" alt="Icon" objectFit="cover" />
+				</CassetteImg>
+				<IncognitoBox></IncognitoBox>
+
+				<IncognitoButton
+					onClick={() => {
+						setShowApp(true);
+					}}
+				>
+					CONTINUE WITHOUT LOGIN
+				</IncognitoButton>
+			</LoginPage>
 		</div>
 	);
 }
---
+++ src/styled-components/Incognito.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const IncognitoContainer = styled.div`
+	align-items: center;
+	width: 300px;
+	height: 250px;
+`;
